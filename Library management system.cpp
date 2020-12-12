9fb701fcd1804ebb2a726b630181d6b1a033cb8a
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2020-12-12 23:45:50 +0530

    Update Library management system.cpp
    optimized  code

--- Library management system.cpp
+++ Library management system.cpp
@@ -3,85 +3,117 @@
 using namespace std;
 #define bookLIMIT 100
 int count = 0;
-
+int countid = 0;
 
 class Library
 {
 
 public:
-
     string bookName[bookLIMIT];
-    
+    int flag = 0;
     int id[bookLIMIT];
 
-    int sizeofarray()
-    {
-        int n = sizeof(bookName) / sizeof(bookName[0]);
-        cout << n << endl;
-        cout << sizeof(bookName) << endl;
-        cout << sizeof(bookName[0]) << endl;
-        return n;
-    }
-
-    void addBook()
+    void adddata()
     {
 
         cout << "Enter the name of book" << endl;
         cin >> bookName[count];
 
         count++;
-    }
-    void deleteBook()
-    {
+        cout << "Enter the id of book" << endl;
+        cin >> id[countid];
 
+        countid++;
+    }
+    void deletedata()
+    {
+        cout << "Enter the book to delete :" << endl;
         string rbn;
         cin >> rbn;
         for (int i = 0; i < count; i++)
         {
             if (bookName[i] == rbn)
             {
-                
+
                 for (int j = i; j < count; j++)
                 {
                     bookName[j] = bookName[j + 1];
-                    
                 }
-                
-                break;
+
+                for (int j = i; j < countid; j++)
+                {
+                    id[j] = id[j + 1];
+                }
+                flag = 1;
+                count--;
+                countid--;
             }
-        
-            else{
-                cout<<"not found"<<endl;
-                
-                
-            }
-        
         }
-        
-        
+
+        if (flag == 0)
+        {
+            cout << "not found" << endl;
+        }
     }
-    void displayBook()
+    void displaydata()
     {
         cout << "books are" << endl;
         for (int i = 0; i < count; i++)
         {
 
             cout << bookName[i] << endl;
-            cout << count << endl;
+            cout << "id are" << endl;
+            cout << id[i] << endl;
         }
+    }
+    //for id//
+
+    void show_Options()
+    {
+        cout << "\n1) Add Book\n";
+        cout << "2) Delet Book\n";
+        cout << "3) Display Books\n";
+        cout << "4) Exit\n";
+        cout << "\nEnter your choice: ";
     }
 };
 
 int main()
 {
     Library c1;
-    c1.addBook();
-    c1.addBook();
-    c1.addBook();
+    while (2 < 3)
+    {
+        int choice;
+        c1.show_Options();
 
-    c1.displayBook();
-    c1.deleteBook();
-    c1.displayBook();
+        cin >> choice;
+        if (choice == 4)
+        {
+            cout << "\nsuccessfully exited!!\n\n";
+            break;
+        }
+        else
+        {
+            switch (choice)
+
+            {
+            case 1:
+                c1.adddata();
+                break;
+            case 2:
+                c1.deletedata();
+                break;
+            case 3:
+                c1.displaydata();
+                break;
+            default:
+                cout << "enter valid choice\n";
+                break;
+            }
+        }
+
+        /* code */
+    }
 
     return 0;
 }
